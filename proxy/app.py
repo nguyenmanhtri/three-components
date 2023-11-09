1f2db5d2fbd6cf7efc5248430e959f8e3ba1c375
Tri Nguyen <tri.mn.nguyen@gmail.com>
2023-11-09 16:49:03 +0700

    init django rest framework

--- proxy/app.py
+++ proxy/app.py
@@ -1,9 +1,11 @@
 import os
 import requests
+import logging
 
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
 API_URL = "http://users:3000"
 
 
@@ -12,11 +14,13 @@ def hello():
     return '<p>Hello world!</p>'
 
 
-@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def proxy():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    logger.info(f'===== Forward request to {path} =====')
     resp = requests.request(
         method=request.method,
-        url=f"{API_URL}",
+        url=f"{API_URL}/{path}",
         headers={key: value for (key, value) in request.headers if key != 'Host'},
         data=request.get_data(),
         cookies=request.cookies,
